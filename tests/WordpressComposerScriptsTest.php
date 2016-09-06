0af0a5b8a43377119d0ae3bd3979394d46ddbd88
Ethan <ethan@webspecdesign.com>
2016-09-06 15:16:53 -0500

    initial commit

---
+++ tests/WordpressComposerScriptsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+// use GuzzleHttp\Client;
+// use BreweryDb\BreweryDb;
+//
+// class BreweryDbTest extends \PHPUnit_Framework_TestCase {
+//
+//   private $breweryDbInstance;
+//   private $key = 'asdfqwer';
+//
+//   function __construct() {
+//     parent::setUp();
+//   }
+//
+//   function setUp() {
+//     $this->breweryDbInstance = new BreweryDb($this->key);
+//   }
+//
+//   public function testsetQueryArgsAddsKey() {
+//     $this->breweryDbInstance->setQueryArgs([]);
+//     $this->assertArrayHasKey('key', $this->breweryDbInstance->getQueryArgs());
+//     $this->assertTrue($this->breweryDbInstance->getQueryArgs()['key'] == $this->key);
+//   }
+
+//}
